245a7a3200f83f6a28de3aac90c3ea43c023a676
David Rodriguez <drodtach@gmail.com>
2015-03-27 17:15:53 -0300

    implementing object behavior

--- main.js
+++ main.js
@@ -6,7 +6,8 @@ var allQuestions = [{question: "Inside which HTML element do we put the JavaScri
 					 		   "js>",
 					 		   "javascript>",
 					 		    "scripting>"],
-					  correctAnswer:0},
+					  correctAnswer:0,
+					  answerChosen:undefined},
 					 {question: "Where is the correct place to insert a JavaScript?",
 					  choices: ["The <body> section",
 					 		   "Both the <head> section and the <body> section are correct",
@@ -18,32 +19,106 @@ var allQuestions = [{question: "Inside which HTML element do we put the JavaScri
 					 		    "script src='xxx.js'>",
 					 		    "script href='xxx.js'>",
 					 		    "script type='xxx.js'>"],
-					  correctAnswer:1},
+					  correctAnswer:1,
+					answerChosen:undefined},
 					 {question: "How do you write \"Hello World\" in an alert box?",
 					  choices: ["msg(\"Hello World\");",
 					 		    "alertBox(\"Hello World\");",
 					 		    "msgBox(\"Hello World\");",
 					 		    "alert(\"Hello World\");"],
-					  correctAnswer:3},
+					  correctAnswer:3,
+					answerChosen:undefined},
 					 {question: "How to write an IF statement in JavaScript?",
 					  choices: ["if i == 5 then",
 					 		    "if i = 5 then",
 					 		    "if i = 5",
 					 		    "if (i == 5)"],
-					  correctAnswer:3},];
+					  correctAnswer:3,
+					answerChosen:undefined},];
+
+
+var questions = {
+	dataQuestions : allQuestions,
+	indexQuestion : 0,
+	currentQuestion : allQuestions[0],
+	totalCorrectly : 0,
+	randomizeQuestions : function(){
+
+	},
+	checkQuestions : function(chosen){
+		if ( chosen == this.dataQuestions[this.indexQuestion] ){
+			totalCorrectly++;
+		}
+	},
+	nextQuestion : function(){
+		if ( this.indexQuestion < this.dataQuestions.length )
+		this.indexQuestion++;
+		console.log('next!');
+		return this.indexQuestion;
+	
+
+	},
+	prevQuestion : function(){
+		if (this.indexQuestion != 0)
+		this.indexQuestion--;
+	},
+	calculateScore : function(){
+
+	}
+} 
+
+var init = function(index){
+	var obj = questions.dataQuestions[index];
+
+	$('p').text(obj.question);
+	var radios = document.getElementsByName('answers');
+	for ( i = 0; i < radios.length; i++){
+
+		$('label[for=ans'+(i+1)+']').html(obj.choices[i]);
+		radios[i].value = obj.choices[i];
+	}
+}
+
+
+var testingObject = function(){
+	init(0);
+
+	questions.dataQuestions = allQuestions;
+	console.log('index:'+questions.indexQuestion)
+//	questions.nextQuestion();
+//	questions.nextQuestion();
+
+
+}
 
 $(document).ready(function(){
-	console.log('ready to improve this!')
+	testingObject();
+
+	/*console.log('ready to improve this!')
 	//randomQuestion();
 	makeQuestions();
-
+*/
+	
 	$('#btn_next').click(function(){
-		checkAnswer(index);
+		
+		init(questions.nextQuestion());
 		
 		
 	});
 
 });
+
+var validateInput = function(){
+
+	var radioButtons = $("#main_form input:radio[name=answers]");
+	var selectedIndex = radioButtons.index(radioButtons.filter(':checked'));
+	if ( selectedIndex == -1){
+		alert('you must selected one answer!')
+		return;
+
+	}
+	return true;
+};
 
 var makeQuestions = function(){
 	index = randomQuestion();
@@ -58,7 +133,7 @@ var displayScore = function(){
 var checkAnswer = function(index){
 
 	var checked  = $('input[name=answers]:checked').index('input[name=answers]');
-	
+	$('input[name=answers]:checked').prop('checked', false);
 
 	if ( checked == allQuestions[index].correctAnswer){
 		totalScore++;
@@ -75,6 +150,8 @@ var checkAnswer = function(index){
 	}else{
 		makeQuestions();
 	}
+
+
 	//console.log(checked.index());
 };
 
